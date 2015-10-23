ab8296c4140750e42dc990b99e9f47ea0ffe71e1
jschroeder <jschroeder@jamasoftware.com>
2015-10-23 07:44:07 -0700

    Jama REST Examples

---
+++ Basic Auth/PHP/using_curl.php
@@ -0,0 +1,17 @@
+# This script uses PHP as a wrapper for curl
+<?php
+$base_url = "{base url}/rest/latest/";
+$username = "API_User";
+$password = "********";
+
+$resource = "projects";
+
+$curl_request = curl_init();
+curl_setopt($curl_request, CURLOPT_URL, $base_url . $resource);
+curl_setopt($curl_request, CURLOPT_USERPWD, "$username:$password");
+
+$result = curl_exec($curl_request);
+curl_close($curl_request);
+
+echo $result;
+?>
